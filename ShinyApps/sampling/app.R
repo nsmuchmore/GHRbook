9274a6566a327900b4c6a509f9298fc8e9068fd9
Amy Finnegan <amy.finnegan@duke.edu>
2017-03-01 08:43:31 -0500

    Added grid to show random sample, rearranged layout

--- ShinyApps/sampling/app.R
+++ ShinyApps/sampling/app.R
@@ -9,11 +9,33 @@ library(ggplot2)
 library(dplyr)
 library(magrittr)
 library(miniUI)
+library(reshape2)
 
-# data
+# data ------------------------------------------------------------------------
 
-dat <- readRDS("/users/amyfinnegan/GHRbook/ShinyApps/sampling/data/dat.rds")
+dat <- readRDS("data/dat.rds")
 dat$race <- as.factor(dat$race)
+levels(dat$race)[levels(dat$race)=="American Indian or Alaska Native"] <- "Other"
+levels(dat$race)[levels(dat$race)=="Black/Negro"] <- "Black"
+
+
+# grid ------------------------------------------------------------------------
+
+# get the x
+x <- seq(1, 317, 1)
+x <- rep(x, 317)
+
+# get the y - repping 1, 2, 3... 317 times
+y <- NULL
+for (i in 1:317) {
+  temp <- rep(i,317)
+  y <- rbind(y, temp)
+}
+
+y <- melt(y)
+
+grid <- data.frame(x=x, y=y$Var2)
+
 
 # colors
 gg_color_hue <- function(n) {
@@ -24,28 +46,34 @@ n = 4
 cols <- gg_color_hue(n)
 
 
-# Define UI for application that draws a histogram
+# Define UI for application 
 ui <- miniPage(
   
   miniTitleBar("Simple Random Sampling"),
   
   miniContentPanel(
+    
    
         align="center",
         
         h4("Use the slider below to select the number of observations to draw at random from
-           the population of North Carolina surveyed in the 1880 Census.  How does the comprability
+           the population of North Carolina surveyed in the 1880 Census.  How does the distribution
            of the sample compare to the population distribution as the number of observations drawn changes?"),
         
         #100 sample
         sliderInput("draw", "Select Size of Draw:",
                     value=500,
                     min=5, max=1000, step=1),
+    
+    splitLayout(cellWidths="100%",
         
         # population
+         plotOutput("grid")),
+    
+    splitLayout(cellWidths = c("30%", "30%", "30%"),
          plotOutput("agePop"),
          plotOutput("sexPop"),
-         plotOutput("racePop")
+         plotOutput("racePop"))
 ))
         
                       
@@ -55,20 +83,10 @@ ui <- miniPage(
 
 
 
-# Define server logic required to draw a histogram
+# Define server logic 
 server <- function(input, output) {
   
-  # I think I want to draw and then cbind them
-  # maybe select the three variables first
-  # then plot by group
-  
-  # N <- reactive({
-  #   
-  #   N <- as.numeric(input$draw)
-  #   
-  #   return(N)
-  #   
-  # })
+
   
 
   
@@ -81,7 +99,7 @@ server <- function(input, output) {
       select(ageN, race, sex) %>%
       mutate(type=rep("Population", length(dat$ageN)))
     
-    set.seed(1)
+    #set.seed(1)
     samp <- dat
     samp <- samp[base::sample(1:nrow(dat), N, replace=FALSE),]
     samp <- samp %>%
@@ -121,7 +139,8 @@ server <- function(input, output) {
       theme(plot.title=element_text(hjust=0.5, size=18),
             axis.text=element_text(size=12),
             axis.title.x=element_blank(),
-            legend.title=element_blank())
+            legend.title=element_blank(),
+            legend.position="top")
     
   })
   
@@ -135,7 +154,8 @@ server <- function(input, output) {
       theme(plot.title=element_text(hjust=0.5, size=18),
             axis.text=element_text(size=12),
             axis.title.x=element_blank(),
-            legend.title=element_blank())
+            legend.title=element_blank(),
+            legend.position="top")
     
   })
   
@@ -149,7 +169,24 @@ server <- function(input, output) {
       theme(plot.title=element_text( hjust=0.5, size=18),
             axis.text=element_text(size=12),
             axis.title.x=element_blank(),
-            legend.title=element_blank())
+            legend.title=element_blank(),
+            legend.position="top")
+    
+  })
+  
+  output$grid <- renderPlot({
+    
+    samp <- grid[base::sample(1:nrow(grid), input$draw, replace=FALSE),]
+    
+    ggplot(grid, aes(x=x, y=y)) +
+      geom_point(col=cols[1], size=0.005) +
+      geom_point(data=samp, col=cols[3]) +
+      labs(title="Population & Sample") +
+      theme_bw() +
+      theme(plot.title=element_text(hjust=0.5, size=18),
+            axis.text=element_blank(),
+            axis.title=element_blank())
+
     
   })
   
